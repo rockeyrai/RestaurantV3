75c967b3c8f9f1aa1ae9717014dd1ccd4f9a6323
Rockey C.R <rockeyrai234@gmail.com>
2025-02-17 15:31:25 +0545

    setting up the server by connecting to mongoose and mysql

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+"use client";
+const express = require("express");
+const cors = require("cors");
+const { connectMongoDB } = require("./database/mongodb");
+const { connectMySql } = require("./database/mysql");
+require("dotenv").config();
+
+// Connect to databases
+connectMongoDB();
+connectMySql();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow cookies
+  })
+);
+app.use(express.raw({ type: "application/json" }));
+
+// Set port with fallback if the environment variable is not set
+const PORT = process.env.MY_PORT || 6000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
